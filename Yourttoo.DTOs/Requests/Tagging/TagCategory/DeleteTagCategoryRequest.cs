96e1d75f95acc8479ec0465fcdc438a3509d8a88
eze <linezekdlr@hotmail.com>
2025-10-23 18:34:29 +0200

    Remove TagCategory

--- Yourttoo.DTOs/Requests/Tagging/TagCategory/DeleteTagCategoryRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Yourttoo.DTOs.Shared.Features;
-
-namespace Yourttoo.DTOs.Requests.Tagging.TagCategory
-{
-    public class DeleteTagCategoryRequest : RequestPayloadBase
-    {
-        public Guid Id { get; set; }
-    }
-}
