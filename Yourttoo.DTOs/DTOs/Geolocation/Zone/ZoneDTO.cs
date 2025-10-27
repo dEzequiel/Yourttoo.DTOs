290e47dedbf79220175449e0692032a809e69155
eze <linezekdlr@hotmail.com>
2025-10-27 13:37:00 +0100

    Isolate DTO

--- Yourttoo.DTOs/DTOs/Geolocation/Zone/ZoneDTO.cs
+++ Yourttoo.DTOs/DTOs/Geolocation/Zone/ZoneDTO.cs
@@ -1,8 +1,15 @@
 
 namespace Yourttoo.DTOs.DTOs.Geolocation.Zone
 {
-    public class ZoneDTO : GeolocationDTO
+    public class ZoneDTO
     {
+        public Guid Id { get; set; }
+        public string Language { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+
         public string PromotionArea { get; set; } = string.Empty;
         public int PromotionAreaPriority { get; set; } = 0;
     }
